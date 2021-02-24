0aab3ec8c6dce9f70f6ad8eb4ae860b4486b7919
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-24 14:19:25 +0530

    Structs demo - 1

--- 02-go-demos/main.go
+++ 02-go-demos/main.go
@@ -13,8 +13,29 @@ func main() {
 	//utils.SwitchDemo()
 	//fmt.Println(utils.Divide(30, 8))
 	//fmt.Println(utils.Sum(3, 1, 4, 2, 5))
-	evenNoGenerator := utils.EvenNumbers()
-	for index := 0; index < 5; index++ {
-		fmt.Println(evenNoGenerator())
-	}
+
+	/* name := ""
+	fmt.Scanf("%s", &name)
+	fmt.Println(name)
+	age := 0
+	fmt.Scanf("%d", &age) */
+	/* %s (string), %d (int), %g (float), %t(bool) */
+	/*
+		fmt.Println(age + 10)
+		evenNoGenerator := utils.EvenNumbers()
+		for index := 0; index < 5; index++ {
+			fmt.Println(evenNoGenerator())
+		}
+	*/
+
+	emp := new(utils.Employee)
+	emp.Id = 100
+	emp.Name = "Magesh"
+	emp.IsEmployed = true
+	emp.City = "Bangalore"
+	fmt.Println(emp)
+
+	emp2 := &utils.Employee{Id: 100, Name: "Magesh", IsEmployed: true, City: "Bangalore "}
+	fmt.Println(emp2)
+
 }
---
+++ 02-go-demos/utils/structs-demo.go
@@ -0,0 +1,12 @@
+package utils
+
+type Employee struct {
+	Id         int
+	Name       string
+	IsEmployed bool
+	City       string
+}
+
+func ToggleEmploymentStatus( /* employee */ ) {
+
+}
