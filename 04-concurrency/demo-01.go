d3067f4825044280ee07d75b5024a0089a171fdc
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-25 15:06:32 +0530

    comm using channels

--- 04-concurrency/demo-01.go
+++ 04-concurrency/demo-01.go
@@ -5,15 +5,19 @@ import (
 	"time"
 )
 
-func say(s string) {
+func say(s string, ch chan string) {
 	/* for i := 0; i < 5; i++ {
 		time.Sleep(100 * time.Millisecond)
 		fmt.Println(s)
 	} */
 	time.Sleep(5000 * time.Millisecond)
+	fmt.Println(s)
+	ch <- "done"
 }
 
 func main() {
-	go say("Hello")
+	ch := make(chan string)
+	go say("Hello", ch)
 	fmt.Println("World")
+	<-ch
 }
